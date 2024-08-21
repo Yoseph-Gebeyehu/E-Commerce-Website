fdc9293e9ef81140fbc3a00d6fb4b14bb3c25d3d
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2024-08-21 18:26:53 +0300

    Add navigation bar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,14 @@ import Contact from "./pages/Contact";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
--- frontend/src/assets/frontend_assets/assets.js
+++ frontend/src/assets/frontend_assets/assets.js
@@ -54,7 +54,7 @@ import p_img50 from "./p_img50.png";
 import p_img51 from "./p_img51.png";
 import p_img52 from "./p_img52.png";
 
-import logo from "./logo.png ";
+import logo from "./logo.png";
 import hero_img from "./hero_img.png";
 import cart_icon from "./cart_icon.png";
 import bin_icon from "./bin_icon.png";
