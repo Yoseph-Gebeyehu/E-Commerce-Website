2ebf14ea7e780e993ce6e5cf70dc92b944684d0a
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2024-08-23 11:25:11 +0300

    Add Footer.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { assets } from "../assets/frontend_assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img className="mb-5 w-32 " src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+            laborum laudantium tenetur omnis eum saepe provident ut, ab ratione
+            animi beatae facilis. Ipsam nobis ab illum cum animi sunt earum!
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5 ">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+251-918-292-773</li>
+            <li>yosephgebeyehu73@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2024 @jo.com - All Rights Reserved
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
