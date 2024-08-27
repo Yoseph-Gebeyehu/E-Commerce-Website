97f0eec1964c9998080106cea42e848c70f068bc
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2024-08-27 14:40:12 +0300

    Creat add to cart feature. Use a toast to show the error.

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -8,6 +9,41 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("Select Product Size");
+      return;
+    }
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    setCartItems(cartData);
+  };
+
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {}
+      }
+    }
+    return totalCount;
+  };
 
   const value = {
     products,
@@ -17,6 +53,9 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
+    cartItems,
+    addToCart,
+    getCartCount,
   };
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -7,7 +7,7 @@ import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, setSize] = useState("");
@@ -89,7 +89,10 @@ const Product = () => {
                   ))}
                 </div>
               </div>
-              <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+              <button
+                onClick={() => addToCart(productData._id, size)}
+                className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+              >
                 ADDD TO CART
               </button>
               <hr className="mt-8 sm:w-4/5" />
