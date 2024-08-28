64f9a56b5cd982b5ae87d5136b645f29e3effc02
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2024-08-28 11:25:52 +0300

    Add login and sign up page.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -38,11 +38,13 @@ const Navbar = () => {
           alt=""
         />
         <div className="group relative">
-          <img
-            className="w-5 cursor-pointer"
-            src={assets.profile_icon}
-            alt=""
-          />
+          <Link to={"/login"}>
+            <img
+              className="w-5 cursor-pointer"
+              src={assets.profile_icon}
+              alt=""
+            />
+          </Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
