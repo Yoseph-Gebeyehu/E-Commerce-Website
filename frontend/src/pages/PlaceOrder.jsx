025ea5a5f1c0b8502e1a399af4157d2f666628aa
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2024-08-28 11:06:37 +0300

    Add orders page.

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
 import { assets } from "../assets/frontend_assets/assets";
+import { ShopContext } from "../context/ShopContext";
 
 const PlaceOrder = () => {
   const [method, setMethod] = useState("cod");
+  const { navigate } = useContext(ShopContext);
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t ">
       {/* ----------------- Left Side -------------------- */}
@@ -113,7 +115,10 @@ const PlaceOrder = () => {
           </div>
 
           <div className="w-full text-end mt-8 ">
-            <button className="bg-black text-white px-16 py-3 text-sm">
+            <button
+              onClick={() => navigate("/orders")}
+              className="bg-black text-white px-16 py-3 text-sm"
+            >
               PLACE ORDER
             </button>
           </div>
