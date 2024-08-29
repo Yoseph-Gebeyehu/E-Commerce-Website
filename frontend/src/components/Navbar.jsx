92571905cf748f84d5ff739de7417f899410b83d
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2024-08-29 14:10:37 +0300

    Update logo design. Increase logo width in NavBar.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to={"/"}>
-        <img src={assets.logo} className="w-36" alt="" />
+        <img src={assets.logo} className="w-48" alt="" />
       </Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
